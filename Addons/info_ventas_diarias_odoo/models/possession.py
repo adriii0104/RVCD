4425b9f295f132e9b040a67cbe367a40b202eab7
Adriel Capellán <124398842+adriii0104@users.noreply.github.com>
2024-09-10 14:44:45 -0400

    Update possession.py

--- Addons/info_ventas_diarias_odoo/models/possession.py
+++ Addons/info_ventas_diarias_odoo/models/possession.py
@@ -2,7 +2,8 @@ from odoo import models, fields, api
 import logging
 from datetime import datetime
 from escpos.printer import Network
-from send_mail import report_sales
+import smtplib
+from email.mime.text import MIMEText
 
 _logger = logging.getLogger(__name__)
 
@@ -222,3 +223,28 @@ RESUMEN POR USUARIO:
 
         # Send the email report
         report_sales(email_content)
+
+def report_sales(body_rec):
+    # Email details
+    sender_email = "reportsendrevel@outlook.com"
+    receiver_email = "adriii0104@hotmail.com"
+    subject = "Reporte de ventas"
+    body = body_rec
+    password = "revelbar2023"
+
+    # Create MIMEText object (plain text email)
+    msg = MIMEText(body, "plain")
+    msg["Subject"] = subject
+    msg["From"] = sender_email
+    msg["To"] = receiver_email
+
+    # Sending the email using SMTP
+    try:
+        # Connect to the SMTP server for Outlook
+        with smtplib.SMTP("smtp-mail.outlook.com", 587) as server:  # Use port 587 for TLS
+            server.starttls()  # Upgrade the connection to secure using TLS
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, msg.as_string())
+        print("Email sent successfully!")
+    except Exception as e:
+        print(f"Error sending email: {e}")
